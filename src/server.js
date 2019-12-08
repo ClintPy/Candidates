f05687e1422729d4656bcb92ff9d53af8ae0c862
Clint Maruti <cmaruti93@gmail.com>
2019-12-08 20:37:29 +0300

    [Starts #002] Adds test

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ import express from 'express'
 import router from '../routes/routes'
 import mongoose from 'mongoose'
 
-const CONNECTION_URL = 'mongodb+srv://root:eastood0009@cluster0-75fsj.mongodb.net/test?retryWrites=true&w=majority'
+const DB_NAME = process.env.DB_NAME
+const DB_PWD = process.env.DB_PWD
+
+const CONNECTION_URL = `mongodb+srv://root:eastood0009@cluster0-75fsj.mongodb.net/test?retryWrites=true&w=majority`
 
 const app = express()
 
 // Conncect to Mongoose and set up connection variable
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-
+ 
 // Added connection for DB connection
 if(!db) 
     console.log("Error connecting db")
